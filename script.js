240b560293a908fdc10ea3d8ae37962da9adc403
Alex <9164819769a@gmail.com>
2018-11-12 13:46:20 +0300

    это наш первый коммит

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+var leftBorderWidth =1;
+let second = 2 ;
+const pi = 3.14;
+console.log(second);
+console.log(4/0);
+
+let persone = {
+    name:'Alex',
+    age:25,
+    isMarried:false
+};
+console.log(persone['age']);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+console.log(arr[2]);
+
+//alert('Hi Wolf');
+//et answer = confirm('are you here &');
+//console.log(answer);
+//let answer = +prompt('Тебе есть 18 ','Да ');
+//console.log(typeof(answer));
+
+let incr=10,
+    decr=10;
+ 
+
+console.log(++incr);
+console.log(--decr);
+
+let isChecked = false,
+    isClose = false;
+    console.log(isChecked  ||  ! isClose);
